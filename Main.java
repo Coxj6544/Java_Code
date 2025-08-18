00a8940d2c315760c716eace2977414eb74e23a7
coxj6 <coxj6544@gmail.com>
2025-08-18 23:19:25 +0100

    Starting to add code to ake sure ive still got this

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+//Imports
+import Objects.Start;
+
 public class Main {
     
+
+    static Start st = new Start("Example");
+    public static void main(String[] args) {
+
+        System.out.println(st.getName());
+    }
 }
---
+++ Objects/Start.java
@@ -0,0 +1,13 @@
+package Objects;
+public class Start {
+    
+    private String name;
+
+    public Start(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
